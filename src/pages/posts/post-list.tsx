25881adf7725b20788170b5973989c21b5954ab9
Annie Huang <annie.hn@gmail.com>
2025-12-27 16:10:29 +1100

    Switch to put the userId as the last column in the table.

--- src/pages/posts/post-list.tsx
+++ src/pages/posts/post-list.tsx
@@ -3,12 +3,12 @@ import { DataTable, List, ReferenceField } from 'react-admin';
 const PostList = () => (
   <List>
     <DataTable>
-      <DataTable.Col source='userId'>
-        <ReferenceField source='userId' reference='users' />
-      </DataTable.Col>
       <DataTable.Col source='id' />
       <DataTable.Col source='title' />
       <DataTable.Col source='body' />
+      <DataTable.Col source='userId'>
+        <ReferenceField source='userId' reference='users' />
+      </DataTable.Col>
     </DataTable>
   </List>
 );
