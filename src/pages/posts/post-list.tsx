618918606d7e20ca03fd415c45a5bbf507ee28ed
Annie Huang <annie.hn@gmail.com>
2025-12-27 16:26:18 +1100

    Can change the label of the <DataTable.Col> field

--- src/pages/posts/post-list.tsx
+++ src/pages/posts/post-list.tsx
@@ -6,8 +6,8 @@ const PostList = () => {
     <List>
       <DataTable>
         <DataTable.Col source='id' />
-        <DataTable.Col source='title' />
         <DataTable.Col source='body' />
+        <DataTable.Col source='title' label='Post Title' />
         <DataTable.Col source='userId'>
           <ReferenceField source='userId' reference='users' />
         </DataTable.Col>
