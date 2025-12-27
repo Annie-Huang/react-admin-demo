0a432ced75200d69db8a71552e58d69e1ef6b3a6
Annie Huang <annie.hn@gmail.com>
2025-12-27 21:01:01 +1100

    Create filters into the user-list.tsx as well

--- src/pages/users/user-list.tsx
+++ src/pages/users/user-list.tsx
@@ -1,8 +1,13 @@
-import { DataTable, EditButton, List } from 'react-admin';
+import { DataTable, EditButton, List, TextInput } from 'react-admin';
 
 const UserList = () => {
+  const postFilters = [
+    <TextInput source='q' label='Search' alwaysOn key='searchKey' />,
+  ];
+
   return (
-    <List>
+    // <List>
+    <List filters={postFilters}>
       <DataTable>
         <DataTable.Col source='id' />
         <DataTable.Col source='name' />
