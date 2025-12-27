dd24789922e19680bac66c1c688c8eab87eb08af
Annie Huang <annie.hn@gmail.com>
2025-12-27 19:35:56 +1100

    Create pages/users/user-show.tsx from console output

---
+++ src/pages/users/user-show.tsx
@@ -0,0 +1,20 @@
+import { EmailField, Show, SimpleShowLayout, TextField } from 'react-admin';
+
+const UserShow = () => {
+  return (
+    <Show>
+      <SimpleShowLayout>
+        <TextField source='id' />
+        <TextField source='name' />
+        <TextField source='username' />
+        <EmailField source='email' />
+        <TextField source='address.street' />
+        <TextField source='phone' />
+        <TextField source='website' />
+        <TextField source='company.name' />
+      </SimpleShowLayout>
+    </Show>
+  );
+};
+
+export default UserShow;
