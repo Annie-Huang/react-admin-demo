87a45a72b44d4df663990cd1bfa4ec0b7159e28d
Annie Huang <annie.hn@gmail.com>
2025-12-27 16:16:29 +1100

    Change back to use <DataTable>

--- src/pages/posts/post-list.tsx
+++ src/pages/posts/post-list.tsx
@@ -4,19 +4,19 @@ const PostList = () => {
   // <List> is the component that will fetch the data from the backend.
   return (
     <List>
-      {/*<DataTable>*/}
-      {/*  <DataTable.Col source='id' />*/}
-      {/*  <DataTable.Col source='title' />*/}
-      {/*  <DataTable.Col source='body' />*/}
-      {/*  <DataTable.Col source='userId'>*/}
-      {/*    <ReferenceField source='userId' reference='users' />*/}
-      {/*  </DataTable.Col>*/}
-      {/*</DataTable>*/}
+      <DataTable>
+        <DataTable.Col source='id' />
+        <DataTable.Col source='title' />
+        <DataTable.Col source='body' />
+        <DataTable.Col source='userId'>
+          <ReferenceField source='userId' reference='users' />
+        </DataTable.Col>
+      </DataTable>
 
-      <SimpleList
-        primaryText={(record) => record.title}
-        secondaryText={(record) => record.body}
-      />
+      {/*<SimpleList*/}
+      {/*  primaryText={(record) => record.title}*/}
+      {/*  secondaryText={(record) => record.body}*/}
+      {/*/>*/}
     </List>
   );
 };
