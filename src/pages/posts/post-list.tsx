d77e82a9c0601ae04a97c9d764902fb45cb6ce1f
Annie Huang <annie.hn@gmail.com>
2025-12-27 16:12:26 +1100

    Add notes

--- src/pages/posts/post-list.tsx
+++ src/pages/posts/post-list.tsx
@@ -1,16 +1,19 @@
 import { DataTable, List, ReferenceField } from 'react-admin';
 
-const PostList = () => (
-  <List>
-    <DataTable>
-      <DataTable.Col source='id' />
-      <DataTable.Col source='title' />
-      <DataTable.Col source='body' />
-      <DataTable.Col source='userId'>
-        <ReferenceField source='userId' reference='users' />
-      </DataTable.Col>
-    </DataTable>
-  </List>
-);
+const PostList = () => {
+  // <List> is the component that will fetch the data from the backend.
+  return (
+    <List>
+      <DataTable>
+        <DataTable.Col source='id' />
+        <DataTable.Col source='title' />
+        <DataTable.Col source='body' />
+        <DataTable.Col source='userId'>
+          <ReferenceField source='userId' reference='users' />
+        </DataTable.Col>
+      </DataTable>
+    </List>
+  );
+};
 
 export default PostList;
