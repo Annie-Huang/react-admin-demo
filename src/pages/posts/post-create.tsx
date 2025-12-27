685ccf605660067e041c8d8d8258831fa16cbd8a
Annie Huang <annie.hn@gmail.com>
2025-12-27 20:27:08 +1100

    Explain the Id field in post-create.tsx

--- src/pages/posts/post-create.tsx
+++ src/pages/posts/post-create.tsx
@@ -3,8 +3,11 @@ import { Create, ReferenceInput, SimpleForm, TextInput } from 'react-admin';
 const PostCreate = () => {
   return (
     <Create>
-      {/* Below is pretty much the same form as post-edit.tsx, except for the id field */}
+      {/* Below is pretty much the same form as post-edit.tsx, except for the id field
+          No point to allow the user to choose the id, the system will just ignore it and use the new id by increasing the highest id by 1
+      */}
       <SimpleForm>
+        {/*<TextInput source='id' />*/}
         <ReferenceInput source='userId' reference='users' />
         <TextInput source='title' />
         <TextInput source='body' multiline rows={5} />
