fd9a7e919e59e9451e7fb5580ab87d37ee0e524c
Annie Huang <annie.hn@gmail.com>
2025-12-27 16:15:50 +1100

    Try <SimpleList> instead of <DataTable>

--- src/pages/posts/post-list.tsx
+++ src/pages/posts/post-list.tsx
@@ -1,17 +1,22 @@
-import { DataTable, List, ReferenceField } from 'react-admin';
+import { DataTable, List, ReferenceField, SimpleList } from 'react-admin';
 
 const PostList = () => {
   // <List> is the component that will fetch the data from the backend.
   return (
     <List>
-      <DataTable>
-        <DataTable.Col source='id' />
-        <DataTable.Col source='title' />
-        <DataTable.Col source='body' />
-        <DataTable.Col source='userId'>
-          <ReferenceField source='userId' reference='users' />
-        </DataTable.Col>
-      </DataTable>
+      {/*<DataTable>*/}
+      {/*  <DataTable.Col source='id' />*/}
+      {/*  <DataTable.Col source='title' />*/}
+      {/*  <DataTable.Col source='body' />*/}
+      {/*  <DataTable.Col source='userId'>*/}
+      {/*    <ReferenceField source='userId' reference='users' />*/}
+      {/*  </DataTable.Col>*/}
+      {/*</DataTable>*/}
+
+      <SimpleList
+        primaryText={(record) => record.title}
+        secondaryText={(record) => record.body}
+      />
     </List>
   );
 };
