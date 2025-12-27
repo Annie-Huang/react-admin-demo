128c52924b4c79a8853be4c637d73df0fd093431
Annie Huang <annie.hn@gmail.com>
2025-12-27 15:50:28 +1100

    Run prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { Admin } from "react-admin";
-import { Layout } from "./Layout";
-import { dataProvider } from "./dataProvider";
+import { Admin } from 'react-admin';
+import { Layout } from './Layout';
+import { dataProvider } from './dataProvider';
 
 export const App = () => (
   <Admin layout={Layout} dataProvider={dataProvider}></Admin>
--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -1,4 +1,4 @@
-import jsonServerProvider from "ra-data-json-server";
+import jsonServerProvider from 'ra-data-json-server';
 
 export const dataProvider = jsonServerProvider(
   import.meta.env.VITE_JSON_SERVER_URL,
