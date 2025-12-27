9d8a422fc97491d50cfe8191f5b98298a0c72b64
Annie Huang <annie.hn@gmail.com>
2025-12-27 15:57:52 +1100

    Add <Resource name='posts' list={ListGuesser} /> into App.tsx to see the table displayed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import { Admin } from 'react-admin';
+import { Admin, ListGuesser, Resource } from 'react-admin';
 import { Layout } from './Layout';
 import { dataProvider } from './dataProvider';
 
 export const App = () => (
-  <Admin layout={Layout} dataProvider={dataProvider}></Admin>
+  <Admin layout={Layout} dataProvider={dataProvider}>
+    {/* The reason we got 'posts' for name because of https://jsonplaceholder.typicode.com/posts
+        After you add below, you can see the posts already displayed in table with sorting and pagination
+        in http://localhost:5173/#/posts
+    */}
+    <Resource name='posts' list={ListGuesser} />
+  </Admin>
 );
